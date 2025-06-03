219fa9f18c3d1bb402336ca895f8103f7d1a2c09
Nina Howley <ninahowley04@gmail.com>
2025-06-03 15:20:29 -0400

    testing

--- testing.py
+++ testing.py
@@ -2,16 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 from datetime import date
+import csv
+import pandas as pd
 
-def get_response(system, user):
+def get_response(model, prompt, input):
     response = requests.post(
         "http://localhost:1234/v1/chat/completions",
         json={
-            # "model": "deepseek/deepseek-r1-0528-qwen3-8b",
-            "model": "deepseek/deepseek-r1-0528-qwen3-8b",
+            "model": model,
             "messages": [
-                {"role": "system", "content": system},
-                {"role": "user", "content": user}
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": input}
             ],
             "character_schema": [
                 {
@@ -42,46 +43,20 @@ def get_response(system, user):
     )
     return response
 
-# Define the expected response structure
-
-#print(response.json())
-#response = response.json()['choices'][0]['message']['content'].split("\n")
-#print(response[-1])
-
-
-def test_bot_urls(urls, system):
-    for url in urls:
-        site = requests.get(url)
-        site.raise_for_status() 
-        text = site.text
-        # meta = str(BeautifulSoup(html, "html.parser").meta)
-        # soup = BeautifulSoup(html, "html.parser")
-        # text = str(soup.find_all("div"))
-
-        # stuff = f"meta: {meta}, text: {text}"
-        
-
-        response = get_response(system, text).json()
-        print(response)
-        response = response['choices'][0]['message']['content'].split("\n")
-
-        print(f"URL: {url}")
-        print(response[-1], "\n")
-        
-# urls = ["https://www.cnn.com/", "https://www.foxnews.com/"]
-# system = "You are a simple credibility checker. Respond in 2 sentences. The first sentence is a credibility score from 1 through 5. The second sentence is a brief reasoning." 
-# test_bot_urls(user, system)
-
-
-def test_bot(user, system):
-        for statement in user:
-            response = get_response(system, statement).json()
+def test_bot(model, inputs, prompt):
+        for input in inputs:
+            response = get_response(model, prompt, input).json()
             response = response['choices'][0]['message']['content'].split("\n")
 
-            print(f"Statement: {statement}")
+            print(f"Statement: {input}")
             print(f"Response: {response[-1]}", "\n")
 
-user = ["Cats are mammals.", "The current year is 2025.", "Joe Biden is president."]
-system = f"You are a simple fact checker. Respond in 2 sentences. The first sentence is True or False. The second sentence is an explanation."
+            with open('tests.csv', 'a', newline='') as outfile:
+                writer = csv.writer(outfile)
+                writer.writerow([model, input, inputs[input], response[-1]])
 
-test_bot(user, system)
+model = "deepseek/deepseek-r1-0528-qwen3-8b"
+inputs = {"Cats are mammals.": True, "Blue is a warm color.": False, "The current year is 2025.":True, "Joe Biden is president.":False}
+prompt = f"You are a simple fact checker. Respond in 2 sentences. The first sentence is True or False. The second sentence is an explanation."
+
+test_bot(model, inputs, prompt)
