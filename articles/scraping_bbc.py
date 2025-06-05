c8427f865313fe92b4606be54867b88c5b6c071b
Nina Howley <ninahowley04@gmail.com>
2025-06-05 15:30:53 -0400

    fixed text retrieval for sport articles

--- articles/scraping_bbc.py
+++ articles/scraping_bbc.py
@@ -3,43 +3,47 @@ from bs4 import BeautifulSoup
 import csv
 from datetime import date
 
-def reset_csv():
-    with open("bbc_articles.csv", "w", newline='', encoding="UTF-8") as outfile:
-        writer = csv.writer(outfile)
-        writer.writerow(['date','type','topic','title', 'url', 'text'])
+# def reset_csv():
+with open("bbc_articles.csv", "w", newline='', encoding="UTF-8") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(['date','type','topic','title', 'url', 'text'])
 
-def run_scraping():
-    html = requests.get("https://www.bbc.com/")
-    soup = BeautifulSoup(html.text, "html.parser")
-    items = soup.find_all(attrs={"data-testid":"dundee-card"})
+# def run_scraping():
+html = requests.get("https://www.bbc.com/")
+soup = BeautifulSoup(html.text, "html.parser")
+items = soup.find_all(attrs={"data-testid":"dundee-card"})
 
-    with open("bbc_articles.csv", "a", newline='', encoding = "UTF-8") as outfile:
-        writer = csv.writer(outfile)
-        for item in items:
+with open("bbc_articles.csv", "a", newline='', encoding = "UTF-8") as outfile:
+    writer = csv.writer(outfile)
+    for item in items:
 
-            link = item.find(attrs={"data-testid":"internal-link"})
-            if link:
-                url = "https://www.bbc.com/" + link['href']
-            else:
-                url = None
-            title = item.find(attrs={"data-testid":"card-headline"}).text
+        link = item.find(attrs={"data-testid":"internal-link"})
+        if link:
+            url = "https://www.bbc.com/" + link['href']
+        else:
+            url = None
+        title = item.find(attrs={"data-testid":"card-headline"}).text
 
-            if url:
-        
-                html = requests.get(url)
-                soup = BeautifulSoup(html.text, "html.parser")
+        if url:
+    
+            html = requests.get(url)
+            soup = BeautifulSoup(html.text, "html.parser")
 
-                parts = url.split("/")
+            parts = url.split("/")
 
-                if "article" in parts or "articles" in parts:
-                    type = "Article"
-                    topic = parts[4].capitalize()
+            if "article" in parts or "articles" in parts:
+                type = "Article"
+                topic = parts[4].capitalize()
+                if topic == "Sport":
+                    text = " ".join([content.text for content in (soup.find_all(attrs={"class":"ssrcss-1q0x1qg-Paragraph e1jhz7w10"}))])
+                else:
                     text = " ".join([content.text for content in (soup.find_all(attrs={"class":"sc-9a00e533-0 hxuGS"}))]) 
-                elif "video" in parts or "videos" in parts:
-                    type = "Video"
-                    topic = parts[4].capitalize()
-                    text = " ".join([content.text for content in (soup.find_all(attrs={"class":"sc-9a00e533-0 hxuGS"}))]) #})[:-2])])
-                writer.writerow([date.today(),type, topic, title, url, text])
+            elif "video" in parts or "videos" in parts:
+                type = "Video"
+                topic = parts[4].capitalize()
+                text = " ".join([content.text for content in (soup.find_all(attrs={"class":"sc-9a00e533-0 hxuGS"}))]) #})[:-2])])
+            writer.writerow([date.today(),type, topic, title, url, text])
 
 #run this once a day
+
 # run_scraping()
